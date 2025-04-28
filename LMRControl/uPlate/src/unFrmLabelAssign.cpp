a84a2a18ff1e90b5262e727719df869f3bf20190
Mauricio Lima <mauricio.lima@loccus.com.br>
2025-04-28 15:57:02 -0300

    feature : refactoring - Remoção de código obsoleto

--- LMRControl/uPlate/src/unFrmLabelAssign.cpp
+++ LMRControl/uPlate/src/unFrmLabelAssign.cpp
@@ -102,88 +102,18 @@ void __fastcall TFrmLabelAssignment::btnImportClick(TObject *Sender)
 //---------------------------------------------------------------------------
 
 
+void __fastcall TFrmLabelAssignment::LabelGridSelectCell(TObject *Sender, int ACol, int ARow, bool &CanSelect)
+{
+	CanSelect = true;
+}
+
+
+//---------------------------------------------------------------------------
+
+
 void __fastcall TFrmLabelAssignment::LabelGridDrawCell(TObject *Sender, int ACol, int ARow, TRect &Rect, TGridDrawState State)
 {
-	TCanvas* canvas = LabelGrid->Canvas;
 	String   content;
-	TColor   color;
-
-	if (State.Contains(gdFixed))
-	{
-		if (ACol == 0 && ARow >= 1)
-			content = AnsiChar('A' + ARow - 1);
-
-		if (ARow == 0 && ACol >= 1)
-			content = IntToStr(ACol);
-
-
-
-		canvas->Brush->Color = LabelGrid->FixedColor;
-		canvas->FillRect(Rect);
-
-		Integer pixelsTextWidth = canvas->TextWidth(content);
-		Integer pixelsTextHeight = canvas->TextHeight(content);
-
-		Integer textPosX = (Rect.Width() / 2 - pixelsTextWidth / 2);
-		Integer textPosY = (Rect.Height() / 2 - pixelsTextHeight / 2);
-
-		LabelGrid->Canvas->Font->Style = TFontStyles() << fsBold;
-		LabelGrid->Canvas->Font->Size  = 12;
-
-		canvas->TextOut(Rect.Left + textPosX, Rect.Top + textPosY, content);
-
-		return;
-	}
-
-	content = "";
-	color = clWhite;
-
-	TWell& well = (*wellMatrixSelected)[ARow - 1][ACol - 1];
-	switch (well.Type)
-	{
-		case TWellType::wlConcentrationStd:
-			content = "STD" + IntToStr(well.ID);
-			color = clYellow;
-			break;
-
-		case TWellType::wlPositiveControl:
-			content = "CP";
-			color = clGreen;
-			break;
-
-			//wlEmpty  = 0,
-	//wlBlank  = 4,
-	//wlNegativeControl = 8,
-	//wlPositiveControl = 12,
-	//wlQualityControl = 16,
-	}
-	//String s = RawValuesGrid->Cells[ACol][ARow];
-
-	if (content.IsEmpty())
-		return;
-
-	//TCanvas *canvas = RawValuesGrid->Canvas;
-
-	canvas->Brush->Color = color;
-	canvas->FillRect(Rect);
-
-	if (State.Contains(gdSelected))
-		canvas->Brush->Color = clSkyBlue;
-
-	if (State.Contains(gdFocused))
-		canvas->DrawFocusRect(Rect);
-
-	canvas->FillRect(Rect);
-
-	Integer pixelsTextWidth = canvas->TextWidth(content);
-	Integer pixelsTextHeight = canvas->TextHeight(content);
-
-	Integer textPosX = (Rect.Width() / 2 - pixelsTextWidth / 2);
-	Integer textPosY = (Rect.Height() / 2 - pixelsTextHeight / 2);
-
-	canvas->TextOut(Rect.Left + textPosX, Rect.Top + textPosY, content);
-{
-	}
 }
 
 
